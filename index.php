032cda95e095fabf2543146c23bf2fba5395fad3
Kevin Plover <kevinplover@gmail.com>
2018-04-20 10:22:53 +0100

    Initial Files

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>    
+    <head>
+      <meta http-equiv="content-type"  content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">  
+        <title>Kevin Plover</title>
+    </head>
+    <body>
+        <header>
+            <div id="canasContainer">
+            <canvas id="myCanvas"></canvas>
+            </div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <div id="hello"></div>
+        </main>
+        <footer></footer>
+    </body>
+        <script src="js/javaScript.js"></script>
+</html>
---
+++ js/javaScript.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "50px Georgia";
+ctx.fillStyle = 'white'
+ctx.fillText("Kevin Plover",0,90);
